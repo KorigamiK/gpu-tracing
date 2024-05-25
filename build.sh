adb0f76559e922b6f9e000015ce17236955eb6a7
korigamik-hypr <korigamik@gmail.com>
2024-05-25 15:59:56 +0530

    chore: Update build process to use build.sh script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+[ -e build/ ] && rm -r build/
+mkdir build/
+cp -r style build/style
+cp -r images build/images
+cp -r book build/book
+cp index.html build/
+cp favicon.ico build/
