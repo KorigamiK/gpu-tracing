fcb8c0377b040b9e50f8dae6e755298e1aa7ac62
korigamik-hypr <korigamik@gmail.com>
2024-05-26 02:21:48 +0530

    fix: remove SSG and deploy pages through branch

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-[ -e build/ ] && rm -r build/
-mkdir build/
-cp -r style build/style
-cp -r images build/images
-cp -r book build/book
-cp index.html build/
-cp favicon.png build/
